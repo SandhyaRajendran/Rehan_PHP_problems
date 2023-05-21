a3df19b61c209f882503f51c5f6f2a18825f7fcc
SandhyaRajendran <sandhyadckap@gmail.com>
2023-05-21 16:57:52 +0530

    array reverse other way completed

---
+++ arrayReverse2.php
@@ -0,0 +1,35 @@
+<?php
+// $array = [5, 2, 1];
+$array = [1, 2, 5];
+// $array = [4, 1, 2, 5];
+$result = [];
+if (isDescendingOrder($array)) {
+    echo "The array is already arranged in descending order.";
+} else {
+    do {
+        for ($i = 0; $i < count($array) - 1; $i++) {
+            // print_r($array[$i]."\n");
+
+            if ($array[$i] < $array[$i + 1]) {
+                $temp = $array[$i + 1];
+                $array[$i + 1] = $array[$i];
+                $array[$i] = $temp;
+                // $result[] = $array;
+                //
+                array_push($result, $array);
+            }
+        }
+    } while (! isDescendingOrder($array));
+    print_r($result);
+}
+function isDescendingOrder($array)
+{
+    for ($i = 0; $i < (count($array) - 1); $i++) {
+        // print_r($array[$i]."\n");
+        if ($array[$i] < $array[$i + 1]) {
+            return false;
+            // print_r($array[$i]."\n");
+        }
+    }
+    return true;
+}
